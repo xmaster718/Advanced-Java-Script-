9470e81e7d2d0a284fc0367dad8271666aff2d97
Aldiyar <rikogurcik856@gmail.com>
2026-09-26 10:33:21 +0600

    done 3 tasks

--- 4week/script.js
+++ 4week/script.js
@@ -1,10 +1,24 @@
 console.log("1 task")
+
 for(let i = 1; i < 11; i++) {
     console.log(i);
 };
 
 console.log("2 task")
-for(let i=11; i > 0; i--){
+
+for(let i=10; i > 0; i--){
     console.log(i);
 }
 
+console.log("3 task")
+
+
+let sum = 0;
+
+for(i=1; i<101; i++){
+    sum +=i;
+}
+console.log(sum);
+
+
+console.log("4 task")
