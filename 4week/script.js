8c5e413e6ca62e041625cb6277e45a16513955dc
Aldiyar <rikogurcik856@gmail.com>
2026-09-26 11:06:15 +0600

    done 6 tasks

--- 4week/script.js
+++ 4week/script.js
@@ -22,3 +22,23 @@ console.log(sum);
 
 
 console.log("4 task")
+
+const list = ["Almaty", "Astana", "Taldykorgan", "Shymkent", "Semey"];
+for(i=0; i<list.length; i++){
+    console.log(i+1, list[i]);
+};
+
+console.log("5 task")
+
+let number = 1;
+
+for (const city of list){
+    console.log(number +'. '+ city);
+    number++;
+}
+
+console.log("6 task")
+
+for (i=2; i<21; i+=2){
+    console.log(i);
+};
